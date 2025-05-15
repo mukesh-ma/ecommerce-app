29a166639a21e270eb900a3bd201f4de1959655b
Ubuntu <ubuntu@ip-172-31-86-208.ec2.internal>
2025-05-15 18:16:16 +0000

    Updated the ecommerce app

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
+
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce';
 
-mongoose.connect('mongodb://localhost:27017/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("MongoDB connection error", err));
 
